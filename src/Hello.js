9e1622d95a1d08605b23cfe5b7d9164be6a63320
jaemin <sonjm3518@dsm.hs.kr>
2022-07-06 11:34:05 +0900

    ⚡ :: 조건부 렌더링 추가

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,16 @@
 import React from "react"; //리액트 컴포넌트를 만들 땐 불러와줘야 한다.
 
-function Hello() {
-    return <div>안녕하세요!</div>
+function Hello({name, color, isSpecial}) {
+   return <div style={{color}}>
+    { isSpecial && <p>자기소개</p>}
+    안녕하십니까 {name}
+    </div>
+}
+
+//기본값 설정하기
+Hello.defaultProps = {
+    name : '알 수 없음',
+    color : 'black'
 }
 
 export default Hello; //Hello 라는 컴포넌트를 내보내겠다는 의미입니다.
