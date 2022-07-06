98fe1f9e5d9ac94ff0e4e87d374dc7f30d641546
jaemin <sonjm3518@dsm.hs.kr>
2022-07-06 19:00:27 +0900

    ⚡ :: InputSample 적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import InputSample from './InputSample'
 
 function App() {
   return (
-    <Wrapper>
-        <Hello name="son" color = "red" isSpecial/>
-        <Hello />
-    </Wrapper>
+    <InputSample></InputSample>
   );
 }
 
