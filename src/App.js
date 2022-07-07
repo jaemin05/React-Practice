5f790214effaa4cee8477296c5624fb847c45cef
jaemin <sonjm3518@dsm.hs.kr>
2022-07-07 19:04:09 +0900

    ⚡ :: 사랑 연결 서비스

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import InputSample from './InputSample'
+import React, { useRef, useState } from 'react';
+import UserList from './UserList'
+import CreateUser from './CreateUser';
 
 function App() {
+
+  const [input, setInput] = useState({
+    accountId: '',
+    password: ''
+  });
+
+  const {accountId, password} = input;
+
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInput({
+      ...input,
+      [name]: value
+    });
+  };
+
+  const [users, setUser] = useState([ 
+  ]);
+
+  const nextId = useRef(1);
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      accountId,
+      password
+    };
+
+    setUser(users.concat(user));
+
+    setInput({
+      accountId:'',
+      password: ''
+    });
+    nextId.current += 1;
+  };
+
   return (
-    <InputSample></InputSample>
-  );
+    <>
+      <CreateUser
+        accountId={accountId}
+        password={password}
+        onChange={onChange}
+        onCreate={onCreate} />
+
+      <UserList users={users} />
+    </>
+  )
 }
 
 export default App;
--- src/UserList.js
+++ src/UserList.js
@@ -3,12 +3,14 @@ import React from "react";
 function User({user}) {
     return (
         <div>
-            <b>{user.accountId}</b> <span>{user.password}</span>
+            <p>
+                <b>{user.accountId}</b>❤️<b>{user.password}</b>
+            </p>
         </div>
     );
 }
 
-function UserList() {
+function UserList({users}) {
     
     return (
         <div>
@@ -16,7 +18,7 @@ function UserList() {
                 <User user={user} key={user.id}/>
             ))}
         </div>
-    )
+    );
 }
 
 export default UserList;
