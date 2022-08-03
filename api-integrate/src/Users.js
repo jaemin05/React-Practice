d7bed19b18a9c137808c91aad7b864943dc747c1
jaemin <sonjm3518@dsm.hs.kr>
2022-08-03 23:37:51 +0900

    ⚡ :: UsersContext 추가

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,37 +1,35 @@
-import React, { useState } from "react";
-import axios from "axios";
-import useAsync from "./useAsync";
+import React, { useState } from 'react';
+import { useUsersState, useUsersDispatch, getUsers } from './UsersContext';
 import User from './User';
-
-async function getUsers() {
-    const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users'
-    );
-    return response.data;
-}
 
 function Users() {
     const [userId, setUserId] = useState(null);
-    const [state, refetch] = useAsync(getUsers, [], true);
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
 
-    const { loading, data: users, error } = state;
+    const { data: users, loading, error } = state.users;
+    const fetchData = () => {
+        getUsers(dispatch);
+    };
 
-    if (loading) return <div>LOADING</div>;
-    if (error) return <div>ERROR</div>;
-    if (!users) return <button onClick={refetch}>LOAD</button>;
+    if (loading) return <div>로딩중..</div>;
+    if (error) return <div>에러가 발생했습니다</div>;
+    if (!users) return <button onClick={fetchData}>불러오기</button>;
 
     return (
         <>
             <ul>
                 {users.map(user => (
-                    <li key={user.id}
+                    <li
+                        key={user.id}
                         onClick={() => setUserId(user.id)}
-                        style={{ cursor: 'pointer' }}>
+                        style={{ cursor: 'pointer' }}
+                    >
                         {user.username} ({user.name})
                     </li>
                 ))}
             </ul>
-            <button onClick={refetch}>RELOAD</button>
+            <button onClick={fetchData}>다시 불러오기</button>
             {userId && <User id={userId} />}
         </>
     );
