a2911adc9f580dad3df27f922be67542a8badad1
jaemin <sonjm3518@dsm.hs.kr>
2022-08-03 23:38:24 +0900

    ⚡ :: UsersContext 추가

--- api-integrate/src/User.js
+++ api-integrate/src/User.js
@@ -1,28 +1,24 @@
-import React from "react";
-import axios from "axios";
-import useAsync from "./useAsync";
+import React, { useEffect } from 'react';
+import { useUsersState, useUsersDispatch, getUser } from './UsersContext';
 
-async function getUser(id) {
-    const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/users/${id}`
-      );
-      return response.data;
-}
+function User({ id }) {
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
+    useEffect(() => {
+        getUser(dispatch, id);
+    }, [dispatch, id]);
 
-function User({id}) {
-    const [state] = useAsync(() => getUser(id), [id]);
+    const { data: user, loading, error } = state.user;
 
-    const {loading, data: user, error} = state;
-    
-    if (loading) return <div>LOAD...</div>;
-    if (error) return <div>ERROR</div>;
+    if (loading) return <div>로딩중..</div>;
+    if (error) return <div>에러가 발생했습니다</div>;
     if (!user) return null;
 
     return (
         <div>
             <h2>{user.username}</h2>
             <p>
-                <b>Email</b> {user.email}
+                <b>Email:</b> {user.email}
             </p>
         </div>
     );
